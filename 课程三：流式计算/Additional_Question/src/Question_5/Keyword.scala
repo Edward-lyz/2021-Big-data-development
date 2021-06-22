9d3597ba048ff5455daf765d7c779b2c1ddb5530
李琰朕 <65755881+Edward-lyz@users.noreply.github.com>
2021-06-22 10:41:41 +0800

    Update Keyword.scala

--- 课程三：流式计算/Additional_Question/src/Question_5/Keyword.scala
+++ 课程三：流式计算/Additional_Question/src/Question_5/Keyword.scala
@@ -5,7 +5,7 @@ import Q5._
 object Keyword extends  App{
   val File: String = "/Volumes/HD/Onedrive/STUDY/大三下/实训/Exercise_3/Additional_Question/src/Question_5/key.txt"
   var line: String = ""
-  var keyword:String =""w
+  var keyword:String =""
   try{
     val br=new BufferedReader (new FileReader (File) )
     try while ( {
